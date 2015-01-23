391e8681f3f30752f7e83ded9143a347af9f336d
Jimmy <zhenyanzhang@anjuke.com>
2015-01-23 13:34:32 +0800

    Multiply Strings

---
+++ Solutions/Multiply Strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param num1, a string
+    # @param num2, a string
+    # @return a string
+    def multiply(self, num1, num2):
+        
+#Multiply Strings
+#https://oj.leetcode.com/problems/multiply-strings/
