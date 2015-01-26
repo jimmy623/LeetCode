7fb73735a06187c1eb44cc0a701be6cb72e294d9
Jimmy <zhenyanzhang@anjuke.com>
2015-01-26 10:17:02 +0800

    Add Binary

--- Solutions/Add Binary.py
+++ Solutions/Add Binary.py
@@ -1,1 +1,9 @@
+class Solution:
+    # @param a, a string
+    # @param b, a string
+    # @return a string
+    def addBinary(self, a, b):
+        str = bin(int(a,2)+int(b,2))
+        return str[2:len(str)]
 #Add Binary
+#https://oj.leetcode.com/problems/add-binary/
