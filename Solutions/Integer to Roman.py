434d403eee4ce429838d97bb779bdf3ce92b3836
Jimmy Zhang <jimmy623@gmail.com>
2015-02-19 21:27:30 +0800

    Roman to Integer

--- Solutions/Integer to Roman.py
+++ Solutions/Integer to Roman.py
@@ -8,16 +8,15 @@ class Solution:
         s10 = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
         s1 = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
         
-        print num  
         i = num/1000
         result += s1000[i]
         num -= i * 1000
         
-        print i
+
         i = num/100
         result += s100[i]
         num -= i*100
-        print i
+
         
         i = num/10
         result += s10[i]
