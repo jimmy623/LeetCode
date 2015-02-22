745727cd10ed4457e31e8a7c0d772e765d0eec82
Jimmy Zhang <jimmy623@gmail.com>
2015-02-22 11:29:04 +0800

    Longest Common Prefix

---
+++ Solutions/Longest Common Prefix.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        n = len(strs)
+        if n == 0:
+            return ""
+        if n == 1:
+            return strs[0]
+        start = strs[0]
+        ls = len(start)
+        length = 0
+        while length < ls:
+            c = start[length]
+            for i in range(1,n):
+                word = strs[i]
+                if not word[length] == c:
+                    return start[0:length]
+            length += 1
+            
+        return start
+#Longest Common Prefix
+#https://oj.leetcode.com/problems/longest-common-prefix/
