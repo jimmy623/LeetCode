968b4859675bcff81ad2777450b3d2f88d8fa90f
Jimmy Zhang <jimmy623@gmail.com>
2015-02-23 15:03:15 +0800

    Pow(x, n)

---
+++ Solutions/Pow(x, n).py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param x, a float
+    # @param n, a integer
+    # @return a float
+    def pow(self, x, n):
+        return x**n
+#Pow(x, n)
+#https://oj.leetcode.com/problems/powx-n/
