ab5ad670b823c675299cee44c8b58e4f26f66c0f
Jimmy <zhenyanzhang@anjuke.com>
2015-05-11 14:37:57 +0800

    Count Primes

---
+++ Solutions/Count Primes.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countPrimes(self, n):
+        isPrime = [True] * max(n, 2)
+        isPrime[0], isPrime[1] = False, False
+        x = 2
+        while x * x < n:
+            if isPrime[x]:
+                p = x * x
+                while p < n:
+                    isPrime[p] = False
+                    p += x
+            x += 1
+        return sum(isPrime)
+#Count Primes
+#https://leetcode.com/problems/count-primes/
