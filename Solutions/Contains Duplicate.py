89c45a02e7a730ead90bae98a462a1f502154c5f
Jimmy <zhenyanzhang@anjuke.com>
2015-06-16 15:34:56 +0800

    Contains Duplicate

---
+++ Solutions/Contains Duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {boolean}
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+#Contains Duplicate
+#https://leetcode.com/problems/contains-duplicate/
