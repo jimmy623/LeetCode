7ca047b1b6c6156eb8d2fb9a920bc3547952e4cd
Jimmy <Jimmy@Jimmys-MacBook-Pro.local>
2016-05-20 11:16:44 +0800

    Shortest Palindrome

---
+++ Solutions/Shortest Palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+# @param {string} s
+# @return {string}
+    def shortestPalindrome(self, s):
+        r = s[::-1]
+        for i in range(len(s) + 1):
+            if s.startswith(r[i:]):
+                return(r[:i] + s)
+#Shortest Palindrome
+#https://leetcode.com/problems/shortest-palindrome/
