c121fd67dbf99a46ee104ffc504f648297333337
Jimmy <jimmy623@gmail.com>
2016-05-20 15:59:06 +0800

    KMP Solution to Shortest Palindrome

--- Solutions/Shortest Palindrome.py
+++ Solutions/Shortest Palindrome.py
@@ -2,9 +2,20 @@ class Solution:
 # @param {string} s
 # @return {string}
     def shortestPalindrome(self, s):
-        r = s[::-1]
-        for i in range(len(s) + 1):
-            if s.startswith(r[i:]):
-                return(r[:i] + s)
+        A=s+"*"+s[::-1]
+        print A
+        cont=[0]
+        for i in range(1,len(A)):
+            index=cont[i-1]
+            print i,index,cont
+            while(index>0 and A[index]!=A[i]):
+                index=cont[index-1]
+            cont.append(index+(1 if A[index]==A[i] else 0))
+        return s[cont[-1]:][::-1]+s
+
+s = Solution()
+data = "abccbaeuf"
+print s.shortestPalindrome(data)
+
 #Shortest Palindrome
 #https://leetcode.com/problems/shortest-palindrome/
