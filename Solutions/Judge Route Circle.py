9a907bcfc1a257902b4c40554a04e8b51e103de8
Jimmy Zhang <jimmy623@gmail.com>
2018-08-30 21:39:14 +0800

    Judge Route Circle

---
+++ Solutions/Judge Route Circle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")
+        
+
+#Judge Route Circle
+#https://leetcode.com/problems/judge-route-circle/description/
