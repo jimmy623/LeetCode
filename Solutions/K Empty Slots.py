33a49822d2f36c9db30330d47798893d2e49bc21
Jimmy Zhang <jimmy623@gmail.com>
2018-09-07 22:42:39 +0800

    K Empty Slots

---
+++ Solutions/K Empty Slots.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def kEmptySlots(self, flowers, k):
+        active = []
+        for day, flower in enumerate(flowers, 1):
+            i = bisect.bisect(active, flower)
+            for neighbor in active[i-(i>0):i+1]:
+                if abs(neighbor - flower) - 1 == k:
+                    return day
+            active.insert(i, flower)
+        return -1
+
+#K Empty Slots
+#https://leetcode.com/problems/k-empty-slots/description/
